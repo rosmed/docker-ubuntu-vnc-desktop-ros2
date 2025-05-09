a692ee7e42fb967b8772cf6bf2d7d57486b93af8
Junichi Tokuda <tokuda@bwh.harvard.edu>
2025-05-09 14:16:20 -0400

    Update for ismr2025

--- slicer/install-segmentation-unet.bash
+++ slicer/install-segmentation-unet.bash
@@ -3,8 +3,8 @@
 export MODULE_DIR=/root/slicer/modules/aigt/SlicerExtension/LiveUltrasoundAi/SegmentationUNet
 
 # if the file exists, then run it
-if [ -f /root/slicer/Slicer-5.2/Slicer ]; then
-    /root/slicer/Slicer-5.2/Slicer  --no-main-window --no-splash --python-script /root/slicer/install-module.py
+if [ -f /root/slicer/Slicer-5.8/Slicer ]; then
+    /root/slicer/Slicer-5.8/Slicer  --no-main-window --no-splash --python-script /root/slicer/install-module.py
 elif [ -f /root/slicer/Slicer-SuperBuild-Release/Slicer-build/Slicer ]; then
     /root/slicer/Slicer-SuperBuild-Release/Slicer-build/Slicer  --no-main-window --no-splash --python-script /root/slicer/install-module.py
 fi
--- slicer/start-slicer-ros2.bash
+++ slicer/start-slicer-ros2.bash
@@ -3,8 +3,8 @@
 source /root/ros2_ws/install/setup.bash
 
 # if the file exists, then run it
-if [ -f /root/slicer/Slicer-5.2/Slicer ]; then
-    /root/slicer/Slicer-5.2/Slicer
+if [ -f /root/slicer/Slicer-5.8/Slicer ]; then
+    /root/slicer/Slicer-5.8/Slicer
 elif [ -f /root/slicer/Slicer-SuperBuild-Release/Slicer-build/Slicer ]; then
     /root/slicer/Slicer-SuperBuild-Release/Slicer-build/Slicer
 fi
